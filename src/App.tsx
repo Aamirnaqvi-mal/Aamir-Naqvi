f7bc4df8eb68ba5b6a3134df179d6800e65384e8
Aamirnaqvi-mal <aamirnaqvi300@gmail.com>
2026-06-03 16:38:10 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -376,9 +376,7 @@ function App() {
               >
                 AAMIRNAQVI03@GMAIL.COM
               </a>
-              <p className="text-[#181f22] text-xl md:text-1xl lg:text-2xl ibm-font mb-0 text-center">
-  Let's create something that actually works.
-</p>
+        
             </div>
 
             {/* Whatsapp */}
@@ -394,9 +392,7 @@ function App() {
               >
                 WHATSAPP
               </a>
-              <p className="text-[#181f22] text-xl md:text-1xl lg:text-2xl ibm-font mb-0 text-center">
-          Lets talk more further
-              </p>
+             
             </div>
 
             {/* Instagram */}
@@ -410,9 +406,7 @@ function App() {
               >
                 INSTAGRAM
               </a>
-           <p className="text-[#181f22] text-xl md:text-1xl lg:text-2xl ibm-font mb-0 text-center">
-                Tap in for visuals with purpose. - follow the flow.
-              </p>
+          
             </div>
           </div>
         </div>
