7a0e384950cb476e83e50dde53e2cc64a53eadca
Aamirnaqvi-mal <aamirnaqvi300@gmail.com>
2026-06-09 14:02:18 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -433,7 +433,7 @@ function App() {
 
           {/* Bottom disclaimer */}
          
-            <p className="text-center ibm-font text-xs mt-12 pb-4" style={{ color: 'var(--warm-gray)' }}>
+            <p className="text-center ibm-font text-xs mt-12 pb-4" 
               All content is original work. Brands and clients belong to their respective owners.
             </p>
           </ClickWrapper>
