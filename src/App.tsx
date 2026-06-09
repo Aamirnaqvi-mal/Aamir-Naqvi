efe6666ccaf0d0e70487832eacfc9e7e25ca067b
Aamirnaqvi-mal <aamirnaqvi300@gmail.com>
2026-06-09 14:04:48 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -434,8 +434,7 @@ function App() {
           {/* Bottom disclaimer */}
         
           
-            className="interactive-disclaimer"
-          >
+          
             <p className="text-center ibm-font text-xs mt-12 pb-4" >
               All content is original work. Brands and clients belong to their respective owners.
             </p>
